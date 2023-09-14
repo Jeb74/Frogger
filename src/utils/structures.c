fd58f478ef389529bd3c198d1150a21cbd65bb22
MRC_CODE <marcolacivita400@gmail.com>
2023-09-14 18:11:44 +0200

    Fixing circular inclusion

---
+++ src/utils/structures.c
@@ -0,0 +1,21 @@
+#include "../headers/structures.h"
+
+
+
+
+
+Package pack(RequestType rqtype, ContentType cntype, void *arg) {
+    Package pkg = (Package) calloc(1, sizeof(Package));
+
+    (*pkg).rqtype = CALLOC(RequestType, 1);
+    (*(*pkg).rqtype) = rqtype;
+
+    (*pkg).cntype = CALLOC(ContentType, 1);
+    (*(*pkg).cntype) = cntype;
+
+    if (cntype == GAMEPKG) (*pkg).arg.targ = arg;
+    else (*pkg).arg.garg = arg;
+
+    return pkg;     
+}
+
